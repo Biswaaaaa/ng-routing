e30ae9f3366706ff1843b0d83da498c70374271a
Biswaaaaa <ibbarman@gmail.com>
2020-01-18 23:27:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-routing';
+  title = "Our Router App";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { RouterModule } from '@angular/router';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    CharactersListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+     { path: 'movies',
+      component: MoviesListComponent
+    },
+    { path: 'characters',
+      component: CharactersListComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
